ffb9d23b4ea0213bcb9a154db571fd4e53cc2636
Alfredo <maicol.13alfrdo@hotmail.com>
2023-06-15 17:12:39 -0500

    subiendo archivos

---
+++ server.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+let products = [
+  {
+    id: 1,
+    name: "laptop",
+    price: 3000,
+  },
+];
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get("/products", (req, res) => {
+  res.json(products);
+});
+
+app.post("/products", (req, res) => {
+  const newProduct = { ...req.body, id: products.length + 1 };
+  products.push(newProduct);
+  res.send(newProduct);
+});
+
+app.put("/products/:id", (req, res) => {
+  const newData = req.body;
+  const productsFound = products.find(
+    (product) => product.id === parseInt(req.params.id)
+  );
+
+  if (!productsFound)
+    return res.sendStatus(400).json({
+      message: "Product not found",
+    });
+
+  products.map((p) =>
+    p.id === parseInt(req.params.id) ? { ...p, ...newData } : p
+  );
+
+  res.json({
+    message: "Product updated sucessfully",
+  });
+});
+
+app.delete("/products/:id", (req, res) => {
+  const productsFound = products.find(
+    (product) => product.id === parseInt(req.params.id)
+  );
+
+  if (!productsFound)
+    return res.sendStatus(400).json({
+      message: "Product not found",
+    });
+
+  products = products.filter((p) => p.id !== parseInt(req.params.id));
+
+  res.sendStatus(204);
+});
+
+app.get("/products/:id", (req, res) => {
+  console.log(req.params.id);
+  const productsFound = products.find((p) => p.id === parseInt(req.params.id));
+
+  if (!productsFound)
+    return res.sendStatus(400).json({
+      message: "Product not found",
+    });
+
+  console.log(productsFound);
+  res.json(productsFound);
+});
+
+app.listen(3000);
+console.log(`server on port ${3000} `);
